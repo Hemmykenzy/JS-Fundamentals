ef571f97a175d568eacb19940ea0acd57ff45b64
Adjewodo Emmanuel <emmanueladjewodo08@gmail.com>
2025-05-25 21:57:04 +0100

    made changes to my code

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,1 +1,9 @@
-console.log("No argument");
+const args = process.argv.length;
+
+if (args <= 2) {
+  console.log("No argument");
+} else if (args === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
